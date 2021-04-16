37c1fcc802b9926bc68dbe569ec9a268b2dc722c
Alex5 <320532235787532>
2021-04-16 19:30:57 +0300

    add recipes cart

--- src/components/IngredientsResults/IngredientsResult.jsx
+++ src/components/IngredientsResults/IngredientsResult.jsx
@@ -1,57 +1,76 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import RecipesCart from "../RecipesCart/RecipesCart";
 
 import "./IngredientsResult.scss";
 
-import { Route, useHistory } from "react-router-dom";
+import { Route, Switch, useHistory, Link } from "react-router-dom";
 
 const IngredientsResult = ({ recipesState }) => {
   const history = useHistory();
+  const [resultOptions, setResultOptions] = useState(null);
 
   return (
-    <Route path="/ingredients/result">
-      <div className="ingredients-result">
-        <div className="ingredients-result__header">
-          <div>
-            <h1>Найдено {recipesState.length} рецептов</h1>
+    <Switch>
+      <Route path={`/ingredients/result/:id`}>
+        <RecipesCart resultOptions={resultOptions} />
+      </Route>
+      <Route path="/ingredients/result">
+        <div className="ingredients-result">
+          <div className="ingredients-result__header">
+            <div>
+              <h1>Найдено {recipesState.length} рецептов</h1>
+            </div>
+            <div className="ingredients-result__header_change-btn">
+              <button onClick={history.goBack}>Изменить ингредиенты</button>
+            </div>
           </div>
-          <div className="ingredients-result__header_change-btn">
-            <button onClick={history.goBack}>Изменить ингредиенты</button>
-          </div>
-        </div>
-        <div className="ingredients-result__body">
-          {recipesState &&
-            recipesState.map((item) => (
-              <div key={item._id} className="ingredients-result__body-item">
-                <div className="ingredients-result__body-item__img">
-                  <img
-                    style={{ backgroundSize: "cover" }}
-                    src={item.recipeCover}
-                    alt=""
-                  />
-                </div>
-                <div className="ingredients-result__body-item__body">
-                  <div>
-                    <div className="ingredients-result__body-item__body-name">
-                      {item.name}
+          <div className="ingredients-result__body">
+            {recipesState &&
+              recipesState.map((item) => (
+                <div key={item._id} className="ingredients-result__body-item">
+                  <div className="ingredients-result__body-item__img">
+                    <img
+                      style={{ backgroundSize: "cover" }}
+                      src={item.recipeCover}
+                      alt=""
+                    />
+                  </div>
+                  <div className="ingredients-result__body-item__body">
+                    <div>
+                      <div className="ingredients-result__body-item__body-name">
+                        {item.name}
+                      </div>
+                      <div className="ingredients-result__body-item__body-lists">
+                        <ul>
+                          <li>{item.ingredients.length} ингредиентов</li>
+                          <li>{item.portionsCount} порций</li>
+                          <li>{item.recipeTime} минут</li>
+                        </ul>
+                      </div>
                     </div>
-                    <div className="ingredients-result__body-item__body-lists">
-                      <ul>
-                        <li>{item.ingredients.length} ингредиентов</li>
-                        <li>{item.portionsCount} порций</li>
-                        <li>{item.recipeTime} минут</li>
-                      </ul>
+                    <div className="ingredients-result__body-item__body-open-btn">
+                      <Link
+                        onClick={() =>
+                          setResultOptions({
+                            id: item._id,
+                            ingredients: item.ingredients,
+                          })
+                        }
+                        to={{
+                          pathname: `/ingredients/result/${item._id}`,
+                          state: { id: item._id, ingredients: item.ingredients },
+                        }}
+                      >
+                        Открыть
+                      </Link>
                     </div>
                   </div>
-
-                  <div className="ingredients-result__body-item__body-open-btn">
-                    <button>Открыть</button>
-                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
-      </div>
-    </Route>
+      </Route>
+    </Switch>
   );
 };
 
---
+++ src/components/RecipesCart/RecipesCart.jsx
@@ -0,0 +1,74 @@
+import { React, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Loader from "../../services/Loader";
+
+const RecipesCart = ({ resultOptions }) => {
+  const [instruction, setInstruction] = useState({});
+  const [instructionLoad, setInstructionLoad] = useState(true);
+
+  let history = useHistory();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://intense-reef-89831.herokuapp.com/getRecipeInstruction/?id=${resultOptions.id}&source=eda.ru`
+      )
+      .then((res) => {
+        console.log(res);
+        setInstruction(res.data);
+        setInstructionLoad(false);
+      });
+  }, [resultOptions]);
+
+  return (
+    <div>
+      {instructionLoad ? (
+        <Loader />
+      ) : (
+        <div>
+          <div>
+            <button onClick={history.goBack}>Назад</button>
+          </div>
+          <div>
+            <h1>{instruction.name}</h1>
+            <small>
+              Источник: <a href="https://eda.ru/">eda.ru</a>
+            </small>
+          </div>
+          <div style={{ display: "flex" }}>
+            {instruction.photos.map((item) => (
+              <img height="400px" src={item} alt="" />
+            ))}
+          </div>
+          <div>
+            <p>{instruction.desription}</p>
+          </div>
+          <div>
+            Ингредиенты:
+            <ul>
+              {resultOptions.ingredients.map((item) => (
+                <li>
+                  {item.name} {item.count} {item.countName}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <div>Способ приготовления:</div>
+            <div>
+              {instruction.steps.map((step, index) => (
+                <div key={index}>
+                  <strong>{index}.</strong>
+                  <p>{step}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RecipesCart;
