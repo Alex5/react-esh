4cebc677e89f0c6cf0c5987d5b6ec34bc2d9f2cf
Roman Zabolotskikh <kanefron5@gmail.com>
2021-05-23 01:35:50 +0300

    Add User-Identity header to all requests for targeting

--- src/api/Api.js
+++ src/api/Api.js
@@ -5,22 +5,40 @@ const instance = axios.create({
     baseURL: "https://intense-reef-89831.herokuapp.com",
     params: {
         mode: 1,
-    }
+    },
+    headers: localStorage.getItem("User-Identity") ? {
+        "User-Identity": localStorage.getItem("User-Identity")
+    } : {},
 });
+
+const setUUID = (res) => {
+    const uuid = res.headers["user-identity"];
+    localStorage.setItem("User-Identity", uuid)
+}
 
 export const searchAPI = {
     getIngredients(inputValue) {
-        return instance.get(`getIngredients/?q=${inputValue}`);
+        return instance.get(`getIngredients/?q=${inputValue}`).then(res => {
+            setUUID(res);
+            return res;
+        });
     },
     getRecipes(newChipItems) {
-        return instance.post(`getRecipes/`, newChipItems)
+        return instance.post(`getRecipes/`, newChipItems).then(res => {
+            setUUID(res);
+            return res;
+        })
     },
     getRecipesOnInput(value) {
-        return instance.get(`getRecipes/?q=${value}&count=5&offset=1`)
+        return instance.get(`getRecipes/?q=${value}&count=5&offset=1`).then(res => {
+            setUUID(res);
+            return res;
+        })
     },
     getIngAndRecipes(value) {
         return Promise.all([this.getIngredients(value), this.getRecipesOnInput(value)])
             .then( (results) => {
+                setUUID(results);
                 return [results[0], results[1]]
             });
     }
